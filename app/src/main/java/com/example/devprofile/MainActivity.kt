00fd288ff11dca6d864cdfea19f22cf545c4674a
Danfrost15 <dhulkiflugarba@gmail.com>
2020-04-19 16:57:03 +0100

    Finished Tablet Layout

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val Rounded = RoundedBitmapDrawableFactory.create(resources,Bitmap)
+        Rounded.cornerRadius = 20f
+        Logo.setImageDrawable(Rounded)
     }
 }
